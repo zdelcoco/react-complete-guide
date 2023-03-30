586ec2888d0b121b87992395aef965b1ad3f14ad
Zachary Delcoco <zdelcoco@gmail.com>
2023-03-30 16:11:58 -0500

    Adding form inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,14 @@
+import {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = "Changed!";
+    setTitle('Updated!');
   };
 
   return (
