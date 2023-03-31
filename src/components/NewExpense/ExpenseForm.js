4613dd385f80fdb175c80e42c8b7cbfe14de7f33
Zachary Delcoco <zdelcoco@gmail.com>
2023-03-30 21:54:13 -0500

    blah

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,8 +7,23 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
 
+  // alternative approach, using one state
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
+
   const titleChangeHandler = (event) => {    
     setEnteredTitle(event.target.value);
+    // alternative approach
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // });
+    // setUserInput( (prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
   const amountChangeHandler = (event) => {
