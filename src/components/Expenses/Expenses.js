d650f159a408e791cfe0ad2ad24b6a0772b9adad
Zachary Delcoco <zdelcoco@gmail.com>
2023-04-04 16:23:06 -0500

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,41 +9,49 @@ const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear] = useState('2023'); // could refactor this to point to current year
 
+  const [rendered, setRendered] = useState('');             // this is so the full list of expenses displays first, then filters down after filter selection
+
   const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    setRendered('True');
   };
 
   const filteredExpenses = props.expenseItems.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(filteredExpense => (
+      <ExpenseItem 
+        key={filteredExpense.id}
+        title={filteredExpense.title} 
+        amount={filteredExpense.amount} 
+        date={filteredExpense.date}
+      />
+    ))};
+
+    let displayExpense = props.expenseItems.map(expense => (
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}
+      />
+    ));
+
+    if (rendered) {
+      displayExpense = expensesContent;
+    }
+
   return(
     <Card className="expenses">
-
       <ExpensesFilter 
         onChangeFilter={changeFilterHandler}
         defaultDate={filteredYear}
       />
-
-      {/* alternate way
-      {props.expenseItems.filter(expense => expense.date.getFullYear().toString() === filteredYear).map(filteredExpense =>
-        <ExpenseItem 
-          key={filteredExpense.id}
-          title={filteredExpense.title} 
-          amount={filteredExpense.amount} 
-          date={filteredExpense.date}
-        />
-      )} */}
-
-      {filteredExpenses.map(filteredExpense =>
-        <ExpenseItem 
-          key={filteredExpense.id}
-          title={filteredExpense.title} 
-          amount={filteredExpense.amount} 
-          date={filteredExpense.date}
-        />
-      )}
-      
+      {displayExpense}      
     </Card>
   );
 
