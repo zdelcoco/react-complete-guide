d8d7fbdedc76c78900ec951dffe9200f43a1400f
Zachary Delcoco <zdelcoco@gmail.com>
2023-04-05 12:49:18 -0500

    conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import './Expenses.css';
 
@@ -9,41 +9,13 @@ const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear] = useState('2023'); // could refactor this to point to current year
 
-  const [rendered, setRendered] = useState('');             // this is so the full list of expenses displays first, then filters down after filter selection
-
   const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    setRendered('True');
   };
 
   const filteredExpenses = props.expenseItems.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(filteredExpense => (
-      <ExpenseItem 
-        key={filteredExpense.id}
-        title={filteredExpense.title} 
-        amount={filteredExpense.amount} 
-        date={filteredExpense.date}
-      />
-    ))};
-
-    let displayExpense = props.expenseItems.map(expense => (
-      <ExpenseItem 
-        key={expense.id}
-        title={expense.title} 
-        amount={expense.amount} 
-        date={expense.date}
-      />
-    ));
-
-    if (rendered) {
-      displayExpense = expensesContent;
-    }
 
   return(
     <Card className="expenses">
@@ -51,7 +23,7 @@ const Expenses = (props) => {
         onChangeFilter={changeFilterHandler}
         defaultDate={filteredYear}
       />
-      {displayExpense}      
+      <ExpensesList items={filteredExpenses}/>     
     </Card>
   );
 
